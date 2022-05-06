4841bddf1b5361bcba84ee86411070a08437e5b6
Wayne <waynew@property24.com>
2022-05-06 17:13:14 +0200

    Code fixes, add first pixel

--- designs.js
+++ designs.js
@@ -1,15 +1,22 @@
 // Select color input
 // Select size input
-const color = document.getElementId("colorPicker");
+const color = document.getElementById("colorPicker");
 const height = document.getElementById("inputHeight");
 const width = document.getElementById("inputWidth");
-const picket = document.getElementById("sizePicker");
+const picker = document.getElementById("sizePicker");
 const canvas = document.getElementById("pixelCanvas");
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+  for (let i = 0; i < height.value; i += 1) {
+    const row = canvas.insertRow(0);
+    for (let j = 0; j < width.value; j += 1) {
+      row.insertCell(0);
+    }
+}
 
 // Your code goes here!
 
 }
+makeGrid();
