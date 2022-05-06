6e85929a594c9520b17ec71ab7e17f75c943904c
Wayne <waynew@property24.com>
2022-05-06 16:41:24 +0200

    Adding variables

--- designs.js
+++ designs.js
@@ -1,5 +1,10 @@
 // Select color input
 // Select size input
+const color = document.getElementId("colorPicker");
+const height = document.getElementById("inputHeight");
+const width = document.getElementById("inputWidth");
+const picket = document.getElementById("sizePicker");
+const canvas = document.getElementById("pixelCanvas");
 
 // When size is submitted by the user, call makeGrid()
 
